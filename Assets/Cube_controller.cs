5eee977c8b5a8a92707edae6d13ccc617f4aeedd
ltenerowicz <lukasz.tenerowicz@itsilesia.com>
2014-03-11 11:03:42 +0100

    color change

--- Assets/Cube_controller.cs
+++ Assets/Cube_controller.cs
@@ -1,13 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using TouchScript.Gestures;
 
 public class Cube_controller : MonoBehaviour {
+	
+MeshRenderer cubeMaterial;	
 
+	
 	// Use this for initialization
 	void Start () {
+		GetComponent<TapGesture>().StateChanged += HandleStateChanged;
+		cubeMaterial = GetComponent<MeshRenderer>();
+	}
+
+	void HandleStateChanged (object sender, TouchScript.Events.GestureStateChangeEventArgs e)
+	{
+			
+		if(cubeMaterial.renderer.material.color == Color.red)
+		cubeMaterial.renderer.material.color = Color.blue;
+		else 
+		cubeMaterial.renderer.material.color = Color.red;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+			
+	
 	}
 }
