92363933b195a2e963172dcc8a644feea14d2fa0
Zygfryd Wieszok <zygfryd.wieszok@itsilesia.com>
2014-03-11 10:56:52 +0100

    Rotation Buttons

--- Assets/Cube_controller.cs
+++ Assets/Cube_controller.cs
@@ -1,13 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Cube_controller : MonoBehaviour {
+public class Cube_controller : MonoBehaviour
+{
+
+    public bool RotateCube;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+	    RotateCube = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	    if (RotateCube)
+	    {
+            transform.Rotate(new Vector3(30,30,10) * Time.deltaTime);
+	    }
 	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 70, 100, 20), "Stop Rotation"))
+        {
+            RotateCube = false;
+        }
+
+        if (GUI.Button(new Rect(10, 110, 100, 20), "Start Rotation"))
+        {
+            RotateCube = true;
+        }
+    }
 }
